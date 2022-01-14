bf31e51e7bbc2f95ad63481d3a66f16482fa89fa
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2022-01-14 16:40:39 -0500

    Add files via upload

---
+++ sitefiles/cardScript.js
@@ -0,0 +1,28 @@
+let playerhand = [];
+let dealerhand = [];
+ 
+function shuffleMe(){cardDeck.sort(function (a, b) {return Math.random() - 0.5;})};
+
+
+function pushOne(){
+shuffleMe();
+
+
+
+var roll = Math.floor(Math.random() * cardDeck.length) + 1;
+
+playerhand.push({"card":cardDeck[roll].card,"suit":cardDeck[roll].suit,"color":cardDeck[roll].color,"eval":cardDeck[roll].eval,"altEval":cardDeck[roll].altEval,"img":cardDeck[roll].img})
+
+
+cardDeck.splice(roll,1,);
+
+console.log(playerhand);
+console.log(cardDeck);
+}
+
+
+////reference
+ ///playerHand.push({"card":cardDeck[roll].card,"suit":cardDeck[roll].suit,"color":cardDeck
+
+//[roll].color,"eval":cardDeck[roll].eval,"altEval":cardDeck[roll].altEval,"img":cardDeck[roll].img})
+        //  cardDeck.splice(roll,1,);
