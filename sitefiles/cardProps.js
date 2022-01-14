468c0984212d0f03ca06ccaabde0b22f44712a36
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2022-01-14 14:49:09 -0500

    Create cardProps.js

---
+++ sitefiles/cardProps.js
@@ -0,0 +1,421 @@
+var coverCard =[{"img":'<img src ="./cards/covercard.png" id="coverCard">'}];
+
+var cardDeck = [{
+    "card": "Ace",
+    "suit": "Diamonds",
+    "color": "Red",
+    "eval": 1,
+    "altEval":11,
+    "img": '<img src ="./cards/Ad.png">'
+  },
+  {
+    "card": "Ace",
+    "suit": "Spades",
+    "color":"Black",
+    "eval": 1,
+    "altEval":11,
+    "img": '<img src ="./cards/As.png">'
+  },
+  {
+    "card": "Ace",
+    "suit": "Clubs",
+    "color":"Black",
+    "eval": 1,
+    "altEval":11,
+    "img": '<img src ="./cards/Ac.png">'
+  },
+  {
+    "card": "Ace",
+    "suit": "Hearts",
+    "color":"Red",
+    "eval": 1,
+    "altEval":11,
+    "img": '<img src ="./cards/Ah.png">'
+  },
+  {
+    "card": "Two",
+    "suit": "Diamonds",
+    "color": "Red",
+    "eval": 2,
+    "altEval":2,
+    "img": '<img src ="./cards/2d.png">'
+  },
+  {
+    "card": "Two",
+    "suit": "Spades",
+    "color": "Black",
+    "eval": 2,
+    "altEval":2,
+    "img": '<img src ="./cards/2s.png">'
+  },
+  {
+    "card": "Two",
+    "suit": "Clubs",
+    "color":"Black",
+    "eval": 2,
+    "altEval":2,
+    "img": '<img src ="./cards/2c.png">'
+  },
+  {
+    "card": "Two",
+    "suit": "Hearts",
+    "color":"Red",
+    "eval": 2,
+    "altEval":2,
+    "img": '<img src ="./cards/2h.png">'
+  },
+  {
+    "card": "Three",
+    "suit": "Diamonds",
+    "color": "Red",
+    "eval": 3,
+    "altEval":3,
+    "img": '<img src ="./cards/3d.png">'
+  },
+  {
+    "card": "Three",
+    "suit": "Spades",
+    "color": "Black",
+    "eval": 3,
+    "altEval":3,
+    "img": '<img src ="./cards/3s.png">'
+  },
+  {
+    "card": "Three",
+    "suit": "Clubs",
+    "color":"Black",
+    "eval": 3,
+    "altEval":3,
+    "img": '<img src ="./cards/3c.png">'
+  },
+  {
+    "card": "Three",
+    "suit": "Hearts",
+    "color":"Red",
+    "eval": 3,
+    "altEval":3,
+    "img": '<img src ="./cards/3h.png">'
+  },
+  {
+    "card": "Four",
+    "suit": "Diamonds",
+    "color": "Red",
+    "eval": 4,
+    "altEval":4,
+    "img": '<img src ="./cards/4d.png">'
+  },
+  {
+    "card": "Four",
+    "suit": "Spades",
+    "color": "Black",
+    "eval": 4,
+    "altEval":4,
+    "img": '<img src ="./cards/4s.png">'
+  },
+  {
+    "card": "Four",
+    "suit": "Clubs",
+    "color":"Black",
+    "eval": 4,
+    "altEval":4,
+    "img": '<img src ="./cards/4c.png">'
+  },
+  {
+    "card": "Four",
+    "suit": "Hearts",
+    "color":"Red",
+    "eval": 4,
+    "altEval":4,
+    "img": '<img src ="./cards/4h.png">'
+  },
+  {
+    "card": "Five",
+    "suit": "Diamonds",
+    "color": "Red",
+    "eval": 5,
+    "altEval":5,
+    "img": '<img src ="./cards/5d.png">'
+  },
+  {
+    "card": "Five",
+    "suit": "Spades",
+    "color": "Black",
+    "eval": 5,
+    "altEval":5,
+    "img": '<img src ="./cards/5s.png">'
+  },
+  {
+    "card": "Five",
+    "suit": "Clubs",
+    "color":"Black",
+    "eval": 5,
+    "altEval":5,
+    "img": '<img src ="./cards/5c.png">'
+  },
+  {
+    "card": "Five",
+    "suit": "Hearts",
+    "color":"Red",
+    "eval": 5,
+    "altEval":5,
+    "img": '<img src ="./cards/5h.png">'
+  },
+  {
+    "card": "Six",
+    "suit": "Diamonds",
+    "color": "Red",
+    "eval": 6,
+    "altEval":6,
+    "img": '<img src ="./cards/6d.png">'
+  },
+  {
+    "card": "Six",
+    "suit": "Spades",
+    "color": "Black",
+    "eval": 6,
+    "altEval":6,
+    "img": '<img src ="./cards/6s.png">'
+  },
+  {
+    "card": "Six",
+    "suit": "Clubs",
+    "color":"Black",
+    "eval": 6,
+    "altEval":6,
+    "img": '<img src ="./cards/6c.png">'
+  },
+  {
+    "card": "Six",
+    "suit": "Hearts",
+    "color":"Red",
+    "eval": 6,
+    "altEval":6,
+    "img": '<img src ="./cards/6h.png">'
+  },
+  {
+    "card": "Seven",
+    "suit": "Diamonds",
+    "color": "Red",
+    "eval" :7,
+    "altEval":7,
+    "img": '<img src ="./cards/7d.png">'
+  },
+  {
+    "card": "Seven",
+    "suit": "Spades",
+    "color":"Black",
+    "eval" : 7,
+    "altEval":7,
+    "img": '<img src ="./cards/7s.png">'
+  },
+  {
+    "card": "Seven",
+    "suit": "Clubs",
+    "color":"Black",
+    "eval" : 7,
+    "altEval":7,
+    "img": '<img src ="./cards/7c.png">'
+  },
+  {
+    "card": "Seven",
+    "suit": "Hearts",
+    "color":"Red",
+    "eval" : 7,
+    "altEval":7,
+    "img": '<img src ="./cards/7h.png">'
+  },
+  {
+    "card": "Eight",
+    "suit": "Diamonds",
+    "color": "Red",
+    "eval" :8,
+    "altEval":8,
+    "img": '<img src ="./cards/8d.png">'
+  },
+  {
+    "card": "Eight",
+    "suit": "Spades",
+    "color":"Black",
+    "eval" : 8,
+    "altEval":8,
+    "img": '<img src ="./cards/8s.png">'
+  },
+  {
+    "card": "Eight",
+    "suit": "Clubs",
+    "color":"Black",
+    "eval" : 8,
+    "altEval":8,
+    "img": '<img src ="./cards/8c.png">'
+  },
+  {
+    "card": "Eight",
+    "suit": "Hearts",
+    "color":"Red",
+    "eval" : 8,
+    "altEval":8,
+    "img": '<img src ="./cards/8h.png">'
+  },
+  {
+    "card": "Nine",
+    "suit": "Diamonds",
+    "color": "Red",
+    "eval" :9,
+    "altEval":9,
+    "img": '<img src ="./cards/9d.png">'
+  },
+  {
+    "card": "Nine",
+    "suit": "Spades",
+    "color":"Black",
+    "eval" : 9,
+    "altEval":9,
+    "img": '<img src ="./cards/9s.png">'
+  },
+  {
+    "card": "Nine",
+    "suit": "Clubs",
+    "color":"Black",
+    "eval" : 9,
+    "altEval":9,
+    "img": '<img src ="./cards/9c.png">'
+  },
+  {
+    "card": "Nine",
+    "suit": "Hearts",
+    "color":"Red",
+    "eval" : 9,
+    "altEval":9,
+    "img": '<img src ="./cards/9h.png">'
+  },
+  {
+    "card": "Ten",
+    "suit": "Diamonds",
+    "color": "Red",
+    "eval" :10,
+    "altEval":10,
+    "img": '<img src ="./cards/10d.png">'
+  },
+  {
+    "card": "Ten",
+    "suit": "Spades",
+    "color":"Black",
+    "eval" : 10,
+    "altEval":10,
+    "img": '<img src ="./cards/10s.png">'
+  },
+  {
+    "card": "Ten",
+    "suit": "Clubs",
+    "color":"Black",
+    "eval" : 10,
+    "altEval":10,
+    "img": '<img src ="./cards/10c.png">'
+  },
+     {
+    "card": "Ten",
+    "suit": "Hearts",
+    "color":"Red",
+    "eval" : 10,
+    "altEval":10,
+    "img": '<img src ="./cards/10h.png">'
+  },
+  {
+    "card": "Jack",
+    "suit": "Diamonds",
+    "color": "Red",
+    "eval" :10,
+    "altEval":10,
+    "img": '<img src ="./cards/Jd.png">'
+  },
+  {
+    "card": "Jack",
+    "suit": "Spades",
+    "color":"Black",
+    "eval" : 10,
+    "altEval":10,
+    "img": '<img src ="./cards/Js.png">'
+  },
+     {
+    "card": "Jack",
+    "suit": "Clubs",
+    "color":"Black",
+    "eval" : 10,
+    "altEval":10,
+    "img": '<img src ="./cards/Jc.png">'
+  },
+     {
+    "card": "Jack",
+    "suit": "Hearts",
+    "color":"Red",
+    "eval" : 10,
+    "altEval":10,
+    "img": '<img src ="./cards/Jh.png">'
+  },
+  {
+    "card": "Queen",
+    "suit": "Diamonds",
+    "color": "Red",
+    "eval" :10,
+    "altEval":10,
+    "img": '<img src ="./cards/Qd.png">'
+  },
+  {
+    "card": "Queen",
+    "suit": "Spades",
+    "color":"Black",
+    "eval" : 10,
+    "altEval":10,
+    "img": '<img src ="./cards/Qs.png">'
+  },
+     {
+    "card": "Queen",
+    "suit": "Clubs",
+    "color":"Black",
+    "eval" : 10,
+    "altEval":10,
+    "img": '<img src ="./cards/Qc.png">'
+  },
+     {
+    "card": "Queen",
+    "suit": "Hearts",
+    "color":"Red",
+    "eval" : 10,
+    "altEval":10,
+    "img": '<img src ="./cards/Qh.png">'
+  },
+  {
+    "card": "King",
+    "suit": "Diamonds",
+    "color": "Red",
+    "eval" :10,
+    "altEval":10,
+    "img": '<img src ="./cards/Kd.png">'
+  },
+    {
+    "card": "King",
+    "suit": "Spades",
+    "color":"Black",
+    "eval" : 10,
+    "altEval":10,
+    "img": '<img src ="./cards/Ks.png">'
+  },
+     {
+    "card": "King",
+    "suit": "Clubs",
+    "color":"Black",
+    "eval" : 10,
+    "altEval":10,
+    "img": '<img src ="./cards/Kc.png">'
+  },
+     {
+    "card": "King",
+    "suit": "Hearts",
+    "color":"Red",
+    "eval" : 10,
+    "altEval":10,
+    "img": '<img src ="./cards/Kh.png">'
+  }
+    ];
+
+    
