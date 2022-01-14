c95d629d6da24f08b8038098735ce628fa2fa098
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2022-01-14 16:08:58 -0500

    Add files via upload

---
+++ sitefiles/siteScript.js
@@ -0,0 +1,24 @@
+   $(document).ready(function() {
+       $('#dot').click(function() {
+
+       pushOne();
+      });
+  });
+
+
+
+
+
+
+
+
+//////////////////
+//reference code
+
+ $(document).ready(function() {
+       $('.dot').click(function() {
+
+       $('.dot').css({'display':'none'});
+         $('.menuOn').removeClass("hiddenish");
+      });
+  });
