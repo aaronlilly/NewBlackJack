ca8dcfcd1a5bb983fd147a36f3391b7078838465
Aaron Lilly <aaronlilly84@gmail.com>
2022-01-15 21:04:50 -0500

    getting better

--- sitefiles/cardScript.js
+++ sitefiles/cardScript.js
@@ -1,6 +1,10 @@
 let playerhand = [];
 let dealerhand = [];
- 
+
+
+
+
+var coverCard =[{"img":'<img src ="./cards/covercard.png" id="coverCard" style="width: 106px;height:164px;">'}]; 
 function shuffleMe(){cardDeck.sort(function (a, b) {return Math.random() - 0.5;})};
 
 function viewPlayerHand(){
@@ -8,29 +12,45 @@ function viewPlayerHand(){
         for (var i = 0; i < playerhand.length; i++) {
                 document.getElementById("playerHandViewer").innerHTML += playerhand[i].img };
 }
-function viewDealerPartialHand(){
+function CoverDealerPartialHand(){
+        document.getElementById("dealerHandViewer").innerHTML = coverCard[0].img;
+
+}
+function revealDealerPartialHand(){
         document.getElementById("dealerHandViewer").innerHTML = "";
-        for (var i = 0; i < dealerhand.length; i++) {
-                document.getElementById("dealerHandViewer").innerHTML += dealerhand[i].img };
+        document.getElementById("dealerHandViewer").innerHTML = coverCard[0].img;
+        for (var i = 1; i < dealerhand.length; i++) {
+                document.getElementById("dealerHandViewer").innerHTML += dealerhand[i].img;
+        };
 }
        
-function pushOneToPlayer(){
+function initDPlayer(){
         var roll = Math.floor(Math.random() * cardDeck.length);
         playerhand.push({"card":cardDeck[roll].card,"suit":cardDeck[roll].suit,"color":cardDeck[roll].color,"eval":cardDeck[roll].eval,"altEval":cardDeck[roll].altEval,"img":cardDeck[roll].img})
         cardDeck.splice(roll,1,);
         viewPlayerHand()
 }
 
-function pushOneToDealer(){
+function initDDealer(){
         var roll = Math.floor(Math.random() * cardDeck.length);
         dealerhand.push({"card":cardDeck[roll].card,"suit":cardDeck[roll].suit,"color":cardDeck[roll].color,"eval":cardDeck[roll].eval,"altEval":cardDeck[roll].altEval,"img":cardDeck[roll].img})
         cardDeck.splice(roll,1,);
-        viewDealerPartialHand();
+        CoverDealerPartialHand();
+}
+
+function secondCDealer(){
+        var roll = Math.floor(Math.random() * cardDeck.length);
+        dealerhand.push({"card":cardDeck[roll].card,"suit":cardDeck[roll].suit,"color":cardDeck[roll].color,"eval":cardDeck[roll].eval,"altEval":cardDeck[roll].altEval,"img":cardDeck[roll].img})
+        cardDeck.splice(roll,1,);
+        
 }
 
 function initDeal(){
-        pushOneToPlayer();
-        pushOneToDealer();
+        initDPlayer();
+        initDDealer();
+        initDPlayer();
+        secondCDealer();
+        revealDealerPartialHand()
 }
 
 ////reference
