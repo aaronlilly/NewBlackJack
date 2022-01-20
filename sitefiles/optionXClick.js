7455f445732fd531c705131f33ead7b9d52a4e8b
Aaron Lilly <aaronlilly84@gmail.com>
2022-01-20 07:27:22 -0500

    not sure

--- sitefiles/optionXClick.js
+++ sitefiles/optionXClick.js
@@ -1,23 +1,33 @@
 $(document).ready(function() {
     $('#hit').click(function() {
         hitMe();
-        dealerEvalu();
+        //dealerEvalu();
    });
    $('#stand').click(function() {
       standMe();
-      dealerEvalu();
+      //dealerEvalu();
    });
 });
 
 
 function hitMe(){
-   disabl("hit");
-   disabl("stand");
+   if (playerStatus.stand != 
+      "yes")
+      {
+         checkBust("player");
+         if (playerStatus.bust != 
+            "yes")
+         {
+          giveAcard2("player");
+         }
+      };
+
 };
 
 function standMe(){
     disabl("stand");
     disabl("hit");
+    playerStatus.stand = "yes";
     
 };
 
@@ -39,3 +49,31 @@ function enabl(eth){
         $('#hit').prop('disabled', false);
      };
 };
+
+function giveAcard2(who){
+   if (who == "player"){
+      var roll = Math.floor(Math.random() * cardDeck.length);
+      playerhand.push({"card":cardDeck[roll].card,"suit":cardDeck[roll].suit,"color":cardDeck[roll].color,"eval":cardDeck[roll].eval,"altEval":cardDeck[roll].altEval,"img":cardDeck[roll].img})
+      cardDeck.splice(roll,1,);
+      viewPlayerHand();
+   }else if(who == "dealer"){
+      var roll = Math.floor(Math.random() * cardDeck.length);
+        dealerhand.push({"card":cardDeck[roll].card,"suit":cardDeck[roll].suit,"color":cardDeck[roll].color,"eval":cardDeck[roll].eval,"altEval":cardDeck[roll].altEval,"img":cardDeck[roll].img})
+        cardDeck.splice(roll,1,);
+        CoverDealerPartialHand();
+        revealDealerPartialHand();
+   }
+}
+
+function checkBust(who)
+{
+   if (who == "player"){
+      if(playerStatus.bnum  > 21){
+         disablePlayer();
+         playerStatus.points -= 1;
+         playerStatus.bust = "yes";
+         
+      }
+
+   }
+}
--- sitefiles/theBrain.js
+++ sitefiles/theBrain.js
@@ -82,6 +82,6 @@ function playerCheckTurn(){
       //    })
       // }
    
-}
+
 
  
