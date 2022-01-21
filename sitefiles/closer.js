d42f2f1dc284b9209b4126919e3ce0ea62be1d99
Aaron Lilly <aaronlilly84@gmail.com>
2022-01-21 15:01:45 -0500

    h

---
+++ sitefiles/closer.js
@@ -0,0 +1,14 @@
+function whoCloser (){
+    //make sure that both have not busted
+    if (dealerStatus.bust == "yes" && dealerStatus.bust == "yes") 
+    {
+      //do nothing
+    }
+    else if (dealerStatus.bust == "no" && dealerStatus.bust == "no" )
+      { 
+    //    dealerFinal();
+    //    playerFinal();
+    //    //then i evaluate those values
+    //    evaluateFinal();
+      }
+  }
